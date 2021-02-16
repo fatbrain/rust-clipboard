38764a53f884d97ab318e708da3d0f595aed7029
Jonas Jonsson <jj@fatbrain.se>
2021-02-16 14:18:56 +0100

    osx clipboard polling

--- src/osx_clipboard.rs
+++ src/osx_clipboard.rs
@@ -15,30 +15,65 @@ limitations under the License.
 */
 
 use common::*;
-use objc::runtime::{Object, Class};
-use objc_foundation::{INSArray, INSString, INSObject};
-use objc_foundation::{NSArray, NSDictionary, NSString, NSObject};
-use objc_id::{Id, Owned};
-use std::error::Error;
+use objc::runtime::{Class, Object};
+use objc_foundation::{INSArray, INSObject, INSString};
+use objc_foundation::{NSArray, NSDictionary, NSObject, NSString};
+use objc_id::{Id, Owned, Shared};
 use std::mem::transmute;
+use std::{error::Error, thread, time::Duration};
+
+#[derive(thiserror::Error, Debug)]
+pub enum OSXError {
+    #[error(r#"Class::get("NSPasteboard")"#)]
+    NSPasteboard,
+    #[error(r#"NSPasteboard#generalPasteboard returned null"#)]
+    NSPasteboardNull,
+    #[error("pasteboard#readObjectsForClasses:options: returned null")]
+    ReturnedNull,
+    #[error("pasteboard#readObjectsForClasses:options: returned empty")]
+    ReturnedEmpty,
+    #[error("NSPasteboard#writeObjects: returned false")]
+    ReturnedFalse,
+}
 
 pub struct OSXClipboardContext {
-    pasteboard: Id<Object>,
+    pasteboard: Id<Object, Shared>,
+}
+
+pub struct ClipboardListener {
+    pasteboard: Id<Object, Shared>,
+    change_count: i64,
+}
+
+impl Iterator for ClipboardListener {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            thread::sleep(Duration::from_millis(100));
+            let change_count: i64 = unsafe { msg_send![self.pasteboard, changeCount] };
+            if change_count != self.change_count {
+                self.change_count = change_count;
+                return Some(0);
+            }
+        }
+    }
 }
 
 // required to bring NSPasteboard into the path of the class-resolver
 #[link(name = "AppKit", kind = "framework")]
 extern "C" {}
 
-impl ClipboardProvider for OSXClipboardContext {
+impl ClipboardProvider<ClipboardListener> for OSXClipboardContext {
     fn new() -> Result<OSXClipboardContext, Box<Error>> {
-        let cls = try!(Class::get("NSPasteboard").ok_or(err("Class::get(\"NSPasteboard\")")));
+        let cls = Class::get("NSPasteboard").ok_or(OSXError::NSPasteboard)?;
         let pasteboard: *mut Object = unsafe { msg_send![cls, generalPasteboard] };
         if pasteboard.is_null() {
-            return Err(err("NSPasteboard#generalPasteboard returned null"));
+            return Err(OSXError::NSPasteboardNull)?;
         }
         let pasteboard: Id<Object> = unsafe { Id::from_ptr(pasteboard) };
-        Ok(OSXClipboardContext { pasteboard: pasteboard })
+        let pasteboard = pasteboard.share();
+        Ok(OSXClipboardContext { pasteboard })
     }
     fn get_contents(&mut self) -> Result<String, Box<Error>> {
         let string_class: Id<NSObject> = {
@@ -51,25 +86,34 @@ impl ClipboardProvider for OSXClipboardContext {
             let obj: *mut NSArray<NSString> =
                 msg_send![self.pasteboard, readObjectsForClasses:&*classes options:&*options];
             if obj.is_null() {
-                return Err(err("pasteboard#readObjectsForClasses:options: returned null"));
+                return Err(OSXError::ReturnedNull)?;
             }
             Id::from_ptr(obj)
         };
         if string_array.count() == 0 {
-            Err(err("pasteboard#readObjectsForClasses:options: returned empty"))
+            Err(OSXError::ReturnedEmpty)?
         } else {
             Ok(string_array[0].as_str().to_owned())
         }
     }
+
     fn set_contents(&mut self, data: String) -> Result<(), Box<Error>> {
         let string_array = NSArray::from_vec(vec![NSString::from_str(&data)]);
         let _: usize = unsafe { msg_send![self.pasteboard, clearContents] };
-        let success: bool = unsafe { msg_send![self.pasteboard, writeObjects:string_array] };
+        let success: bool = unsafe { msg_send![self.pasteboard, writeObjects: string_array] };
         return if success {
             Ok(())
         } else {
-            Err(err("NSPasteboard#writeObjects: returned false"))
+            Err(OSXError::ReturnedFalse)?
         };
+    }
+
+    fn iter(&mut self) -> ClipboardListener {
+        let change_count: i64 = unsafe { msg_send![self.pasteboard, changeCount] };
+        ClipboardListener {
+            pasteboard: self.pasteboard.clone(),
+            change_count,
+        }
     }
 }
 
